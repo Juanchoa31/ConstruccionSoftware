df6ead4155ca4aea93efb8ed19745b81d2cbe06e
Juanchoa31 <juandavidzapata53@gmail.com>
2024-10-09 19:25:23 -0500

    Reader and writer files

--- fileProject/src/main/java/com/mycompany/fileproject/FileProject.java
+++ fileProject/src/main/java/com/mycompany/fileproject/FileProject.java
@@ -1,35 +1,90 @@
 package com.mycompany.fileproject;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
+import java.util.Arrays;
 
 public class FileProject {
-
+    public static ArrayList<User> users = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         Scanner teclado = new Scanner (System.in);
         
-        String fileName = JOptionPane.showInputDialog("Ingrese el nombre del archivo");
+        String fileName = JOptionPane.showInputDialog("Ingrese el nombre del archivo con su extensión");
+        String path = "./" + fileName;
         
-        JOptionPane.showMessageDialog(null, fileName);
+        String opcion = selectMenuOption();
         
-        String path = "./" + fileName + ".fp";
+        switch(opcion){
+            case "1":
+                writeTextToFile(path);
+                break;
+            case "2":
+                readFile(path);
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+    
+    public static void createFile(String path) throws IOException{
         File file = new File(path);
         
-        
         if (file.exists()){
-            JOptionPane.showMessageDialog(null, "Archivo ya existente");
-            System.out.println(file.getName());
-            
-        }else {
+            JOptionPane.showMessageDialog(null, "El archivo ya existe, no se va a crear");
+        }else{
             System.out.println(file.getPath());
             file.createNewFile();
+        }
+    }
+    
+    public static void writeTextToFile(String path) throws IOException{
+        File file = new File(path);
+        FileWriter fr = new FileWriter(file, true);
+        
+        String texto = JOptionPane.showInputDialog("Que quieres escribir?");
+        
+        fr.append(texto+ "\n");
+        fr.close();
+    }
+    
+    public static void readFile(String path)throws FileNotFoundException, IOException{ 
+        File file = new File(path);
+        FileReader fr = new FileReader(file);
+        
+        BufferedReader br = new BufferedReader(fr);
+        
+        String line = null;
+        while((line = br.readLine()) != null){
+            loadUserCollection(line);
+        }
+        br.close();
+        for(User user : users){
+            System.err.println(user.getInfo());
+        }
+    }
+    
+    public static String selectMenuOption(){     
+        String menuTemplate = 
+                """
+                1. Escribir texto en el archivo. 
+                2. Leer texto del archivo""";
+        
+        return JOptionPane.showInputDialog(menuTemplate);    
+    }
+    
+    public static void loadUserCollection(String recordLine){
+        if (!recordLine.contains("USERNAME")){
+            String[] records = recordLine.split(",");
+            User user = new User(records[0], records[1],records[2]);
             
-            FileWriter fr = new FileWriter(file);
-            fr.write("Hola que tal" + "\n");
-            fr.close();
+            users.add(user);
         }
     }
 }
